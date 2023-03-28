8ff615e48218d2301d80182c23fb67cca407a8aa
Pavel Borisov <pashkin.elfe@gmail.com>
2023-03-28 18:09:25 +0400

    chunk recovery workers queue for the case serialized o_table > RECOVERY_QUEUE_BUF_SIZE

--- src/catalog/indices.c
+++ src/catalog/indices.c
@@ -659,23 +659,35 @@ o_define_index(Relation rel, Oid indoid, bool reindex,
 static void
 workers_send_o_table(Pointer o_table_serialized, int o_table_size)
 {
-	RecoveryMsgIdxBuild	*msg;
-	uint64				 msg_size = sizeof(RecoveryMsgHeader) + o_table_size;
-	int 				i;
+	RecoveryMsgIdxBuild *cur_chunk;
+	uint64				sent_size = 0,
+						cur_size,
+						cur_chunk_size;
+	int 				i,
+						nchunks;
 
-	msg = palloc(msg_size);
 	Assert(!(*recovery_single_process));
-	msg->header.type = RECOVERY_PARALLEL_INDEX_BUILD;
-	memcpy(&msg->o_table_serialized, o_table_serialized, o_table_size);
+	cur_chunk = palloc(Min(sizeof(RecoveryMsgHeader) + o_table_size, RECOVERY_QUEUE_BUF_SIZE));
 
-	elog(WARNING, "%lu bytes of o_table send to all recovery workers", msg_size);
-
-	for (i = 0; i < recovery_pool_size_guc; i++)
+	while (sent_size < o_table_size)
 	{
-		worker_send_msg(i, (Pointer) msg, msg_size);
-		worker_queue_flush(i);
+		cur_size = Min(o_table_size - sent_size,
+					RECOVERY_QUEUE_BUF_SIZE - offsetof(RecoveryMsgIdxBuild, o_table_serialized));
+		cur_chunk_size = offsetof(RecoveryMsgIdxBuild, o_table_serialized) + cur_size;
+		cur_chunk->header.type = RECOVERY_PARALLEL_INDEX_BUILD;
+		cur_chunk->o_table_size = (sent_size == 0) ? o_table_size : 0;
+		memcpy(&cur_chunk->o_table_serialized, o_table_serialized + sent_size, cur_size);
+		for (i = 0; i < recovery_pool_size_guc; i++)
+		{
+			worker_send_msg(i, (Pointer) cur_chunk, cur_chunk_size);
+			worker_queue_flush(i);
+		}
+		sent_size += cur_size;
+		nchunks++;
 	}
-	pfree(msg);
+	elog(WARNING, "%lu bytes of o_table sent to all recovery workers in %d chunks", sent_size, nchunks);
+
+	pfree(cur_chunk);
 }
 
 /*
