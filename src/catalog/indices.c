ad63fecfac1a14b7244c0ceafd63ed3c0632037b
Ilya Kobets <i.kobets@orioledb.com>
2023-03-28 20:58:22 +0300

    issue 198 fix

--- src/catalog/indices.c
+++ src/catalog/indices.c
@@ -325,6 +325,9 @@ o_define_index(Relation rel, Oid indoid, bool reindex,
 	int16		indnkeyatts;
 	OBTOptions *options;
 
+	if (!OidIsValid(indoid))
+		return;
+
 	index_rel = index_open(indoid, AccessShareLock);
 	if (context)
 		reuse = OidIsValid(context->oldNode);
