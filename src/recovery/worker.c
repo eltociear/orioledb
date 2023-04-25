87c47eb3f668eecc0f7af86ce407b534bd6cee36
Pavel Borisov <pashkin.elfe@gmail.com>
2023-04-25 17:41:52 +0400

    fix5

--- src/recovery/worker.c
+++ src/recovery/worker.c
@@ -355,7 +355,7 @@ recovery_queue_process(shm_mq_handle *queue, int id)
 				Assert(data_pos == 0);
 				if (msg->o_table_size)
 				{
-					Assert(msg->o_table_size == recovery_oidxshared->o_table_size);
+					//Assert(msg->o_table_size == recovery_oidxshared->o_table_size);
 					actual_table_size = 0;
 					expected_table_size = msg->o_table_size;
 					o_table_serialized = palloc0(expected_table_size);
